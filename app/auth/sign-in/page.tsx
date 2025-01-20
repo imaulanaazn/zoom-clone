f6f505df2085e06e5cdb3d36f596a09d0b9b30ef
irham maulana <i.maulana.azn@gmail.com>
2025-01-20 09:50:56 +0700

    change login page icon

--- app/auth/sign-in/page.tsx
+++ app/auth/sign-in/page.tsx
@@ -6,15 +6,18 @@ export default function SiginInPage() {
   return (
     <main className="flex h-screen w-full items-center justify-center">
       <div className=" flex h-96 flex-col items-center justify-center rounded-xl bg-white p-28">
-        <Image
-          src={"/images/zoom-logo.png"}
-          alt={"zoom logo"}
-          width={200}
-          height={100}
-        />
-        <h1 className="mt-6 text-xl font-bold">LOGIN TO CONVIN</h1>
+        <div className="flex items-center gap-3">
+          <Image
+            src={"/icons/logo.svg"}
+            alt={"zoom logo"}
+            width={50}
+            height={50}
+          />
+          <h2 className="font-bold text-4xl">Convin</h2>
+        </div>
+        <h1 className="mt-6 text-xl font-bold">Login untuk melanjutkan</h1>
         <p className="mb-10 mt-2 text-sm">
-          Login using your zoom account to continue using this website
+          Klik tombol authorize untuk menghubungkan akun zoom
         </p>
         <a
           href={`https://zoom.us/oauth/authorize?response_type=code&client_id=OJ6QysNcQLKVN8XkB1DY8A&redirect_uri=${FRONTEND_URL}/redirect`}

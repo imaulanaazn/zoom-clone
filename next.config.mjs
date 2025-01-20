ccbf4454f58f4f08c7d42186d86215330a283169
irham maulana <i.maulana.azn@gmail.com>
2025-01-20 09:41:49 +0700

    add backend url to next image

--- next.config.mjs
+++ next.config.mjs
@@ -7,6 +7,11 @@ const nextConfig = {
         hostname: "img.clerk.com",
       },
       {
+        protocol: "https",
+        hostname: "zoomserver-production.up.railway.app",
+        port: "8080",
+      },
+      {
         protocol: "http",
         hostname: "localhost",
         port: "4000",
